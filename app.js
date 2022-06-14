f00805b64755bbf1018aa55cc7a816851e9e976d
Lanka Rathnayake <LankaR@99x.io>
2022-06-14 23:52:55 +0530

    initialize

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/user-routes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api", router);
+const port = process.env.PORT || 5001;
+
+
+mongoose
+    .connect(process.env.ATLAS_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("db Connected"));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`); 
+});
+
+ 
