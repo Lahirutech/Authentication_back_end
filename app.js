2ef8bdd982c471aa64955ebdd38f4f72b9a904a6
Lanka Rathnayake <LankaR@99x.io>
2022-06-15 23:03:21 +0530

    adding a middleware

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(cors())
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(function (req, res, next) {   
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
+    res.setHeader('Access-Control-Allow-Credentials', true);    
+    next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
