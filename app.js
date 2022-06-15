f9614453f5983a23bc055c3c02618af913488c58
Lanka Rathnayake <LankaR@99x.io>
2022-06-15 23:20:27 +0530

    modifying the server.js

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
+app.use(function (req, res, next) {   
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
+    // res.setHeader('Access-Control-Allow-Credentials', true);    
+    next();
+});
 
 
 app.use(cookieParser());
