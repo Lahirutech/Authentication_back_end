8a4c52e8efc37a3912abb66894dfb9fab4ba20a2
Lanka Rathnayake <LankaR@99x.io>
2022-06-15 22:58:46 +0530

    adding the  cors policy

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors())
 
-// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
