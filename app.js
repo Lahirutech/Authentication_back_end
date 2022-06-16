ea87053f37fb12c4821ff7b35fa7a57bf6dfb64d
Lanka Rathnayake <LankaR@99x.io>
2022-06-16 21:13:48 +0530

    adding new websites

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const app = express();
 // });
 
 app.use(cors({
-    origin: 'https://62aa1f25e3e65a612b257a02--extraordinary-mermaid-710456.netlify.app',
+    origin: ['https://master--extraordinary-mermaid-710456.netlify.app','*'],
     credentials: true
 }));
 
